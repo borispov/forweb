93c79f21f9b580cfa7ef5f2d65095ac1780f64fb
Boris Povolotsky <boristofu@gmail.com>
2023-09-11 22:09:31 +0300

    Added pricing section - Fixed Netlify FORM by opting out of server rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify/functions";
 
 import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  output: "server",
-  adapter: netlify()
 });
